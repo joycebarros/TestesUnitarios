d96dbd0c67d3cdcf670ffa75d31a29033332e133
Joyce <joyce_barros@yahoo.com.br>
2020-11-13 18:24:59 -0300

    commit atividades 2 e 3.

--- src/test/java/br/com/qualiti/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/qualiti/servicos/LocacaoServiceTest.java
@@ -1,9 +1,8 @@
 package br.com.qualiti.servicos;
 
-import static br.com.qualiti.utils.DataUtils.adicionarDias;
-
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +10,8 @@ import br.com.qualiti.entidades.Filme;
 import br.com.qualiti.entidades.Locacao;
 import br.com.qualiti.entidades.Usuario;
 import br.com.qualiti.utils.DataUtils;
+
+
 
 public class LocacaoServiceTest {
 
@@ -26,45 +27,26 @@ public class LocacaoServiceTest {
 		// CENARIOS: Montagem de ambiente
 		
 		Usuario usuario1 = new Usuario("Joyce");
-		Usuario usuario2 = new Usuario("Elian");
 		Filme filme1 = new Filme("Titanic", 5, 5.5);
-		Filme filme2 = new Filme("Harry Potter", 4, 6.0);
-		Locacao locacao1 = new Locacao();
-	//	Locacao locacao2 = new Locacao();
+				
 		LocacaoService service = new LocacaoService();
 		
 		// ACAO : O teste
 				
-		locacao1.setUsuario(usuario1);
-		locacao1.setFilme(filme2);
-		locacao1.setValor(filme2.getPrecoLocacao());
-		locacao1.setDataLocacao(new Date());
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 2);
-		locacao1.setDataRetorno(dataEntrega);
-		
 		Locacao locacao = null;
 		try {
-			locacao = service.alugarFilme(usuario2, filme1);
+			locacao = service.alugarFilme(usuario1, filme1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		locacao2.setUsuario(usuario2);
-//		locacao2.setFilme(filme1);
-//		locacao2.setValor(filme1.getPrecoLocacao());
-//		locacao2.setDataLocacao(new Date());
-//		Date dataEntrega2 = new Date();
-//		dataEntrega2 = adicionarDias(dataEntrega2, 3);
-//		locacao2.setDataRetorno(dataEntrega2);
 		
 		// VERIFICACAO : Checar resultado do teste
 		
-//		System.out.println(usuario1.toString());
-//		System.out.println(usuario2.toString());
-//		System.out.println(filme1.toString());
-//		System.out.println(filme2.toString());
-		System.out.println(locacao.getValor());
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(locacao.getValor() == 5.5);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		
+		Assert.assertNotNull(locacao);
 		
 		
 	}
